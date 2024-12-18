a224ab6a0a015a1c5d861cdcc5f7415be687cd6c
progman.task <makidevhelp@gmail.com>
2024-12-17 20:41:22 -0800

    Add files via upload

---
+++ orgST_rangen.py
@@ -0,0 +1,15 @@
+diaryversion="1.0"
+supertext="35"or"34"
+
+print("diary custom terminal")
+a=input(">...")
+if a=="explanation":
+    print("what would you like to know?")
+    print("usability, supertext and startext")
+    b=input(">...")
+    if b=="usability":
+        print("you are allowed to use the diary template with credit to progman.task (MakiDev)")
+    if b=="supertext and startext" or "supertext" or "startext":
+              print("supertext and startext are data you can set")
+if a=="vb":
+    print(diaryversion)
